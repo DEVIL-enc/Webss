5b0dea3689d5ef336442090d0e0688f0b1f61982
DEVIL-enc <moahmedjujuj@gmail.com>
2026-06-02 22:08:35 +0200

    Update file1.py

--- file1.py
+++ file1.py
@@ -8,10 +8,10 @@ import random
 import threading
 
 # ضع توكن البوت الخاص بك هنا
-TOKEN = "7987425397:"
+TOKEN = "6496923968:AAEy6aMJeD4uPZIzMqWhjVBJakoWee9sZeo:"
 
 # ضع الـ ID الخاص بك هنا (مهم جداً للوحة التحكم)
-ADMIN_ID = 77729359
+ADMIN_ID = 1707478010
 
 # تم إزالة num_threads لأن البوت أصبح Async ولا يحتاج مسارات متعددة
 bot = AsyncTeleBot(TOKEN)
